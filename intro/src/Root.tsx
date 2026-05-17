16dab6a955433131ca3dc8796ef419a2c7b13bdc
kelsaeed <khaledawad552002@gmail.com>
2026-05-17 23:17:37 +0300

    add droplet nav and splash

--- intro/src/Root.tsx
+++ intro/src/Root.tsx
@@ -1,16 +1,27 @@
 import "./index.css";
 import { Composition } from "remotion";
 import { SoftwaveIntro } from "./SoftwaveIntro";
+import { Splash } from "./Splash";
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="SoftwaveIntro"
-      component={SoftwaveIntro}
-      durationInFrames={110}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Composition
+        id="SoftwaveIntro"
+        component={SoftwaveIntro}
+        durationInFrames={110}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="Splash"
+        component={Splash}
+        durationInFrames={34}
+        fps={30}
+        width={600}
+        height={600}
+      />
+    </>
   );
 };
